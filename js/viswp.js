eea79af91179fb405c419d16fd1dd36ab7e8a82a
rubenal <rubenal@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-05-06 19:54:25 +0000

    git-svn-id: https://plugins.svn.wordpress.org/jquery-visualize-for-wordpress/tags/0.9@381880 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/viswp.js
@@ -0,0 +1,6 @@
+jQuery(function(){
+	jQuery('.v-bar').visualize({type: 'bar', height: '300px', width: '420px'});		
+	jQuery('.v-pie').visualize({type: 'pie', height: '300px', width: '420px'});		
+	jQuery('.v-area').visualize({type: 'area', height: '300px', width: '420px'});		
+	jQuery('.v-line').visualize({type: 'line', height: '300px', width: '420px'});		
+});
